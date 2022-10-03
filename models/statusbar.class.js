8e350e02d966ec367a23b2d42fe224991e99cd8a
Stefan Herrmann <99035284+StefanHerrmann1991@users.noreply.github.com>
2022-10-03 02:49:42 +0200

    better life over 100%

--- models/statusbar.class.js
+++ models/statusbar.class.js
@@ -5,14 +5,12 @@ class Statusbar extends DrawableObject {
         'img/7.Marcadores/Barra/Marcador vida/azul/40_.png',
         'img/7.Marcadores/Barra/Marcador vida/azul/60_.png',
         'img/7.Marcadores/Barra/Marcador vida/azul/80_.png',
-        'img/7.Marcadores/Barra/Marcador vida/azul/100_.png',
-        'img/7.Marcadores/Barra/Marcador vida/Naranja/0_ .png',
+        'img/7.Marcadores/Barra/Marcador vida/azul/100_.png',        
         'img/7.Marcadores/Barra/Marcador vida/Naranja/20__1.png',
         'img/7.Marcadores/Barra/Marcador vida/Naranja/40_ .png',
         'img/7.Marcadores/Barra/Marcador vida/Naranja/60_ .png',
         'img/7.Marcadores/Barra/Marcador vida/Naranja/80_ .png',
-        'img/7.Marcadores/Barra/Marcador vida/Naranja/100_ .png',
-        'img/7.Marcadores/Barra/Marcador vida/verde/0_.png',
+        'img/7.Marcadores/Barra/Marcador vida/Naranja/100_ .png',        
         'img/7.Marcadores/Barra/Marcador vida/verde/20_.png',
         'img/7.Marcadores/Barra/Marcador vida/verde/40_.png',
         'img/7.Marcadores/Barra/Marcador vida/verde/60_.png',
@@ -39,19 +37,22 @@ class Statusbar extends DrawableObject {
     }
 
     whichStatus() {
-        if (this.percentage >= 100) { return 5; }
-        else if (this.percentage > 80) { return 4; }
-        else if (this.percentage > 220) { return 4; }
-        else if (this.percentage > 200) { return 4; }
-        else if (this.percentage > 180) { return 4; }
-        else if (this.percentage > 160) { return 4; }
-        else if (this.percentage > 140) { return 4; }
-        else if (this.percentage > 120) { return 4; }
-        else if (this.percentage > 100) { return 4; }
-        else if (this.percentage > 80) { return 4; }
-        else if (this.percentage > 60) { return 3; }
-        else if (this.percentage > 40) { return 2; }
-        else if (this.percentage > 20) { return 1; }
+        if (this.percentage >= 280) { return 16; }                 
+        else if (this.percentage >= 260) { return 15; }
+        else if (this.percentage >= 260) { return 14; }
+        else if (this.percentage >= 260) { return 13; }
+        else if (this.percentage >= 240) { return 12; }
+        else if (this.percentage >= 220) { return 11; }
+        else if (this.percentage >= 200) { return 10; }
+        else if (this.percentage >= 180) { return 9; }
+        else if (this.percentage >= 160) { return 8; }
+        else if (this.percentage >= 140) { return 7; }
+        else if (this.percentage >= 120) { return 6; }
+        else if (this.percentage >= 100) {return 5; }
+        else if (this.percentage >= 80) { return 4; }
+        else if (this.percentage >= 60) { return 3; }
+        else if (this.percentage >= 40) { return 2; }
+        else if (this.percentage >= 20) { return 1; }
         else { return 0; }
     }
 }
